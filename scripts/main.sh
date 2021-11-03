5c42687b762d8796359695ae696828768d2070a1
Owen Kahn <okahnus@gmail.com>
2021-11-02 20:37:10 -0400

    Update script to next election.

--- scripts/main.sh
+++ scripts/main.sh
@@ -2,8 +2,8 @@
 
 mkdir build
 cd build
-# wget https://results.enr.clarityelections.com//PA/Allegheny/111176/281494/reports/detailxml.zip
-wget https://results.enr.clarityelections.com//PA/Allegheny/109361/277443/reports/detailxml.zip
+wget https://results.enr.clarityelections.com//PA/Allegheny/111176/281494/reports/detailxml.zip
+# wget https://results.enr.clarityelections.com//PA/Allegheny/109361/277443/reports/detailxml.zip
 unzip detailxml.zip
 python3 ../scripts/process_scytl.py
 cd ..
